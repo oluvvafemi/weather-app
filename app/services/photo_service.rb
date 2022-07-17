ef3dcd742763f475af11098d979506a3664a73c8
Chris Adebiyi <oluvvafemi@gmail.com>
2022-07-17 15:54:25 +0200

    Style background image

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -1,11 +1,11 @@
 class PhotoService
   def initialize(city)
-    @city_name = city
+    @city_name = city.delete(' ')
   end
 
   def photo_url
-    response = fetch_photo
-    "#{JSON.parse(response.to_s)['results'][0]['urls']['raw']}&q=70&w=1920&h=1080" if response.code == 200
+    response = JSON.parse(fetch_photo.to_s)
+    "#{response['results'][0]['urls']['raw']}&q=50&w=1920&h=1080" unless response['results'].empty?
   end
 
   private
